eb596ef8dfa503ed1cc4d5fcd1cebd69c5fbe357
sngom1995 <ngomsamba75@gmail.com>
2022-11-20 15:59:56 +0000

    add gestures to MoodItemRow

--- src/components/MoodItemRow.tsx
+++ src/components/MoodItemRow.tsx
@@ -4,54 +4,93 @@ import format from 'date-fns/format';
 import {MoodOptionWithTimestamp} from '../types';
 import {theme} from '../theme';
 import {useAppContext} from '../App.provider';
-import {PanGestureHandler} from 'react-native-gesture-handler';
 import {
-  useAnimatedGestureHandler,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Reanimated, {
   useAnimatedStyle,
   useSharedValue,
+  withTiming,
+  useAnimatedGestureHandler,
+  runOnJS,
 } from 'react-native-reanimated';
 
 type MoodItemRowProps = {
   item: MoodOptionWithTimestamp;
 };
 
+const maxPan = 80;
+
 export const MoodItemRow: React.FC<MoodItemRowProps> = ({item}) => {
   const appContext = useAppContext();
   const offset = useSharedValue(0);
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{translateX: offset.value}],
-  }));
-  const onGestureEvent = useAnimatedGestureHandler(
-    {
-      onActive: event => {
-        console.warn(event.translationX);
-      },
-    },
-    [],
-  );
 
   const handlePress = React.useCallback(() => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     appContext.handleDeleteMood(item);
   }, [appContext, item]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: offset.value}],
+  }));
+
+  const removeWithDelay = React.useCallback(() => {
+    setTimeout(() => {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      appContext.handleDeleteMood(item);
+    }, 250);
+  }, [appContext, item]);
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {shouldRemove: boolean}
+  >(
+    {
+      onActive: (event, ctx) => {
+        const xVal = Math.floor(event.translationX);
+        offset.value = xVal;
+
+        // use Absolute value so the user could swipe either left or right
+        if (Math.abs(xVal) <= maxPan) {
+          ctx.shouldRemove = false;
+        } else {
+          ctx.shouldRemove = true;
+        }
+      },
+      onEnd: (_, ctx) => {
+        if (ctx.shouldRemove) {
+          // if the item should be remove, animate it off the screen first
+          offset.value = withTiming(Math.sign(offset.value) * 2000);
+
+          // then trigger the remove mood item with a small delay
+          runOnJS(removeWithDelay)();
+        } else {
+          // otherwise, animate the item back to the start
+          offset.value = withTiming(0);
+        }
+      },
+    },
+    [],
+  );
+
   return (
     <PanGestureHandler
       minVelocityX={1}
       minVelocityY={100}
-     // onGestureEvent={onGestureEvent}
-     >
-      <View style={styles.moodItem}>
+      onGestureEvent={onGestureEvent}>
+      <Reanimated.View style={[styles.moodItem, animatedStyle]}>
         <View style={styles.iconAndDescription}>
           <Text style={styles.moodValue}>{item.mood.emoji}</Text>
           <Text style={styles.moodDescription}>{item.mood.description}</Text>
         </View>
-        <Pressable hitSlop={16} onPress={handlePress}>
-          <Text style={styles.deleteText}>Delete</Text>
-        </Pressable>
         <Text style={styles.moodDate}>
           {format(new Date(item.timestamp), "dd MMM, yyyy 'at' h:mmaaa")}
         </Text>
-      </View>
+        <Pressable hitSlop={16} onPress={handlePress}>
+          <Text style={styles.deleteText}>Delete</Text>
+        </Pressable>
+      </Reanimated.View>
     </PanGestureHandler>
   );
 };
@@ -65,7 +104,7 @@ const styles = StyleSheet.create({
   moodDate: {
     textAlign: 'center',
     color: theme.colorLavender,
-    fontFamily: theme.fontFamilyLight,
+    fontFamily: theme.fontFamilyRegular,
   },
   moodItem: {
     backgroundColor: 'white',
@@ -78,7 +117,6 @@ const styles = StyleSheet.create({
   moodDescription: {
     fontSize: 18,
     color: theme.colorPurple,
-    fontWeight: 'bold',
     fontFamily: theme.fontFamilyBold,
   },
   iconAndDescription: {
